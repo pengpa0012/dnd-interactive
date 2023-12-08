0e637e37ae41cd27fc085ec5bc98ca2000c63cfa
Pengpa0012 <peng.ilikha@gmail.com>
2023-12-08 17:09:28 +0800

    added initial dragend element

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-s
 const items = document.querySelectorAll(".item")
+const body = document.querySelector("body")
 
 items.forEach(el => {
-  el.addEventListener("dragend", (e) => console.log(e.x, e.y))
+  el.addEventListener("dragend", (e) => {
+    const {x, y} = e
+    const newEl = document.createElement("div")
+    newEl.textContent = "test"
+    newEl.style.position = `fixed`
+    newEl.style.top = `${y}px`
+    newEl.style.left = `${x}px`
+    newEl.style.transform = `translate(-50%, -50%)`
+    body.appendChild(newEl)
+  })
 })
